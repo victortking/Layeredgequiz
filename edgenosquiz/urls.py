61a907d7e07d3f3487decfac8a9135da1348996f
victortking-procoder <fredrickvictor01@outlook.com>
2025-05-14 12:09:31 +0100

    tweaking urls.py

--- edgenosquiz/urls.py
+++ edgenosquiz/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('quiz/', views.edgenosquiz_view, name='edgenosquiz'),
+    path('', views.edgenosquiz_view, name='edgenosquiz'),
 ]
